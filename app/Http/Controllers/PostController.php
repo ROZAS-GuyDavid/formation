992e57cf5caa33febb1a2a7945ccd1ff25964f9e
ROZAS <rozasguydavid@gmail.com>
2018-09-03 17:34:03 +0200

    Mise en place de delete
    le champs cacher delete a deja été placer au momont de la création de la vue

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -160,6 +160,8 @@ class PostController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $post = Post::find($id);
+        $post->delete();
+        return redirect()->route('post.index')->with('message', 'success delete');
     }
 }
